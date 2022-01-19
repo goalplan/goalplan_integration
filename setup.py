d17ba163c236db8f6d24256756166cca75d27d5a
Gabriel Muj <gabriel@goalplan.com>
2022-01-19 18:05:03 +0200

    Initial implementation for create_import_job cloud function (#1)
    * Initial implementation for create_import_job cloud function
    
    * added readme
    
    * gitignore + readme
    
    * readme update
    
    * Use config.json + add support for multiple import job mappings
    
    * Use config.json + add support for multiple import job mappings
    
    * Added test case for create_import_job
    
    * added library how it should work
    
    * library structure
    
    * Create library structure + refactoring
    
    * Added setup.py
    
    * Move requirements in setup.py
    
    * Add github action to run pytest
    
    * Rename to .yml
    
    * Add new line
    
    * Update README.md
    
    * moved README.md
    
    * Rename create_import_job to example_function
    
    * Update README.md
    
    * gitignore + python-version
    
    * Make dry_run option mandatory to be either True/False
    
    * updated documenation
    
    Co-authored-by: Gabriel Muj <mujgabriel@gmail.com>
    Co-authored-by: Peter Lauri <peterlauri@gmail.com>

---
+++ setup.py
@@ -0,0 +1,16 @@
+from glob import glob
+from setuptools import find_packages, setup
+from os.path import splitext, basename
+
+
+setup(
+    name='goalplan',
+    version='0.1',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    install_requires=[
+        "google-cloud-storage==2.0.0",
+        "requests==2.26.0",
+    ],
+)
