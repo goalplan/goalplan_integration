f05d8261c522b29500dda933a6b3d00f75f33e01
Peter Lauri <peterlauri@gmail.com>
2023-10-18 15:40:17 +0200

    github actions update + docs

--- examples/google_cloud/bucket_cloud_function/example_function/main.py
+++ examples/google_cloud/bucket_cloud_function/example_function/main.py
@@ -15,25 +15,39 @@ def create_import_job(event, context):
     """
     base_url = "https://api.example.com/data/import/"
     api_token = "******"
-    file_mappings = {
-        "data-upload/phone-data": "a23f7a01-d814-452b-8846-5f8d3f70bb09"
-    }
-
+    """
+    file_mappings is a list of file patterns that will trigger upload to Goalplan. Only first match will be processed, 
+    so order is important. Each mapping have three entries:
+    
+    1. Regular expressing matching the path of the uploaded file to the bucket
+    2. The Goalplan data identifier that the file will be mapped to
+    3. Path where the uploaded file will be moved to. If set to null/None file will remain.
+    """
+    file_mappings = [
+        [
+            "drop-folder/match[^/]+.csv",
+            "d02e4c2d-55ef-4074-8029-ca1866246a14",
+            None
+        ]
+    ]
     client = BucketFileImporter(base_url, api_token, file_mappings)
 
     # or read from dictionary
     # config = {
     #     "base_url": "https://api.example.com/data/import/",
     #     "api_token": "******",
-    #     "file_mappings": {
-    #         "data-upload/phone-data": "a23f7a01-d814-452b-8846-5f8d3f70bb09"
-    #     }
+    #     "file_mappings": [
+    #         [
+    #             "drop-folder/match[^/]+.csv",
+    #             "d02e4c2d-55ef-4074-8029-ca1866246a14",
+    #             None
+    #         ]
+    #     ]
     # }
-    # BucketFileImporter.from_dict(config)
+    # client = BucketFileImporter.from_dict(config)
 
     # or read from json config file
     # client = BucketFileImporter.from_config_file('config.json')
 
     # Use dry_run=False once you want to go live
     client.handle_event(event, context, dry_run=True)
-
