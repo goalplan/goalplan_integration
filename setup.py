396456cf111f61ea43221b2c9c466283fe613311
Peter Lauri <peterlauri@gmail.com>
2023-10-18 15:49:30 +0200

    update setup version

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from os.path import splitext, basename
 
 setup(
     name='goalplan',
-    version='0.1',
+    version='0.2',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
     install_requires=[
-        "google-cloud-storage==2.0.0",
-        "requests==2.26.0",
+        "google-cloud-storage==2.12.0",
+        "requests==2.31.0",
     ],
 )
