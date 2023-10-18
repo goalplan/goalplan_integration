ac67b02e23f52f3906fd39c74d8b9985edd76d08
Peter Lauri <peterlauri@gmail.com>
2023-10-18 23:16:49 +0200

    python311

--- examples/google_cloud/bucket_cloud_function/example_function/main.py
+++ examples/google_cloud/bucket_cloud_function/example_function/main.py
@@ -1,20 +1,16 @@
+import os
+
 from goalplan.google import BucketFileImporter
 
 
 def create_import_job(event, context):
-    """Background Cloud Function to be triggered by Cloud Storage.
-       This generic function logs relevant data when a file is changed.
-    Args:
-        event (dict):  The dictionary with data specific to this type of event.
-                       The `data` field contains a description of the event in
-                       the Cloud Storage `object` format described here:
-                       https://cloud.google.com/storage/docs/json_api/v1/objects#resource
-        context (google.cloud.functions.Context): Metadata of triggering event.
-    Returns:
-        None; the output is written to Stackdriver Logging
-    """
-    base_url = "https://api.example.com/data/import/"
-    api_token = "******"
+    base_url = "https://api.goalplan.com/data/import/"
+    api_token = os.environ.get('GOALPLAN_DATA_IMPORT_TOKEN')
+    dry_run = True
+
+    if api_token is None and not dry_run:
+        raise Exception('GOALPLAN_DATA_IMPORT_TOKEN env variable needs to be set, preferably using secrets.')
+
     """
     file_mappings is a list of file patterns that will trigger upload to Goalplan. Only first match will be processed, 
     so order is important. Each mapping have three entries:
@@ -26,28 +22,9 @@ def create_import_job(event, context):
     file_mappings = [
         [
             "drop-folder/match[^/]+.csv",
-            "d02e4c2d-55ef-4074-8029-ca1866246a14",
+            "11111111-2222-3333-4444-555555555",
             None
         ]
     ]
     client = BucketFileImporter(base_url, api_token, file_mappings)
-
-    # or read from dictionary
-    # config = {
-    #     "base_url": "https://api.example.com/data/import/",
-    #     "api_token": "******",
-    #     "file_mappings": [
-    #         [
-    #             "drop-folder/match[^/]+.csv",
-    #             "d02e4c2d-55ef-4074-8029-ca1866246a14",
-    #             None
-    #         ]
-    #     ]
-    # }
-    # client = BucketFileImporter.from_dict(config)
-
-    # or read from json config file
-    # client = BucketFileImporter.from_config_file('config.json')
-
-    # Use dry_run=False once you want to go live
-    client.handle_event(event, context, dry_run=True)
+    client.handle_event(event, context, dry_run=dry_run)
